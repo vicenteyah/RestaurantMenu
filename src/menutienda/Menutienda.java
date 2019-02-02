c2651db4dd0a2a7c7950bf64c9fc5c59b7d40180
DavidMendoza5 <cruz-azul99@hotmail.com>
2019-02-01 18:00:17 -0600

    initial commit

---
+++ src/menutienda/Menutienda.java
@@ -0,0 +1,14 @@
+
+package menutienda;
+
+
+public class Menutienda {
+
+    
+    public static void main(String[] args) {
+        ventana1 v1 = new ventana1();
+        v1.setVisible(true);
+        v1.setLocationRelativeTo(null);
+    }
+    
+}
